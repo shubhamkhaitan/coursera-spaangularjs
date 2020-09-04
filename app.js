5ccc0e5a42d42426c8f8ccbc78b2d40c26115ce6
shubhamkhaitan <65844445+shubhamkhaitan@users.noreply.github.com>
2020-09-04 14:38:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,32 +4,24 @@
 
     LCController.$inject = ['$scope'];
     function LCController($scope) {
-        $scope.name = {};
+        $scope.name = "";
+        $scope.message = "";
         
         $scope.displayMessage = function () {
-            var totalCount = calculateLengthString("$scope.name")
-            if (totalCount == 0)
-            {
-                return "Please enter data first";
+            if($scope.name === "") {
+                $scope.message = "Please enter data first";
             }
-            elseif (totalCount > 0 && totalCount <=3)
-            {
-                return "Enjoy!";
-            }
-            elseif (totalCount > 3)
-            {
-                return "Too much!";
+            else {
+                var $nameArray = $scope.name.split(",");
+                var $length = $nameArray.length;
+                if ($length <= 3)
+                {
+                    $scope.message = "Enjoy!";
+                }
+                else {
+                    $scope.message = "Too much!";
+                }
             }
         };
-        
-        function calculateLengthString(str) {
-            var string = str.split(", ");
-            var cnt = 0;
-            for(i=0; i<string.length; i++)
-            {
-                cnt = cnt+1;
-            }
-            return cnt;
-        }    
     }
 }) ();
